48e30ddd822a66ed11e0b41aeb441628be21272f
Nathan Dizon-Mapula <ndizonmapula@gmail.com>
2021-06-27 23:12:07 -0400

    adding vet website v2 for lab 6

---
+++ SEG3125_Lab6/scripts/validations.js
@@ -0,0 +1,47 @@
+function validatePhone(txtPhone) {
+    var a = document.getElementById(txtPhone).value;
+    // This filter asks for something like (12345), so parentheses with any number (at least 1)
+    // of digits
+    var filter = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+$(document).ready(function(){
+
+    // phone validation, it calls validatePhone
+    // and also some feedback as an Alert + putting a value in the input that shows the format required
+    // the "addClass" will use the class "error" defined in style.css and add it to the phone input
+    // The "error" class in style.css defines yellow background and red foreground
+    $("#phone").on("change", function(){
+        if (!validatePhone("phone")){
+            alert("Wrong format for phone");
+            $("#phone").val("(xxx) xxx-xxxx");
+            $("#phone").addClass("error");
+        }
+        else {
+            $("#phone").removeClass("error");
+        }
+    });
+
+    // To change the style of the calender, look in jqueryui.com, under Themes, in the ThemeRoller Gallery
+    // You can try different themes (the names are under the calendars) / This is Excite Bike
+    // To use a different theme you must include its css in your HTML file.
+    // The one I included in my HTML is the Excite Bike, but you can try others
+
+    // Also, here is a good tutorial for playing with the datepicker in https://webkul.com/blog/jquery-datepicker/
+    // Datepicker is also documented as one of the widgets here: https://api.jqueryui.com/category/widgets/
+    $( "#date" ).datepicker(
+        {
+            dateFormat: setDateFormat,
+            // no calendar before June 1rst 2020
+            minDate: new Date('06/01/2020'),
+            maxDate: '+4M',
+        }
+    );
+
+});
